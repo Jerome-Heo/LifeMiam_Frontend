1c00a990ab70ad9fca898b15af9d6ee766ef0816
Elisa <elisafan8@gmail.com>
2024-08-06 17:17:18 +0200

    ca passe ou pas

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,5 @@
 import {
+  View,
   Image,
   KeyboardAvoidingView,
   Platform,
@@ -6,57 +7,38 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-} from 'react-native';
+} from "react-native";
 
 export default function HomeScreen({ navigation }) {
-
+  handleGo = () => {
+    navigation.navigate("TabNavigator", { screen: "Search" });
+  };
 
   return (
-    <Provider store={store}>
-     <Head>
-       <title>Best friends</title>
-     </Head>
-     <Component {...pageProps} />
-   </Provider>
-  )
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+      <TouchableOpacity style={styles.go} onPress={() => handleGo()}>
+        <Text style={styles.textButt}>Go To Search</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "green",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  image: {
-    width: '100%',
-    height: '50%',
+  go: {
+    backgroundColor: "red",
+    width: 200,
+    height: 200,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  title: {
-    width: '80%',
-    fontSize: 38,
-    fontWeight: '600',
-  },
-  input: {
-    width: '80%',
-    marginTop: 25,
-    borderBottomColor: '#ec6e5b',
-    borderBottomWidth: 1,
-    fontSize: 18,
-  },
-  button: {
-    alignItems: 'center',
-    paddingTop: 8,
-    width: '80%',
-    marginTop: 30,
-    backgroundColor: '#ec6e5b',
-    borderRadius: 10,
-    marginBottom: 80,
-  },
-  textButton: {
-    color: '#ffffff',
-    height: 30,
-    fontWeight: '600',
-    fontSize: 16,
+  textButt: {
+    color: "white",
   },
 });
