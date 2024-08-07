e1ffa05b9898dc9df3b1dad5e2639f8d80707f63
jérôme <jplusquellec@gmail.com>
2024-08-07 09:46:26 +0200

    rename AddButton et GoBackButton

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -17,10 +17,10 @@ export default function SearchScreen({ navigation }) {
   const [recipes, setRecipes] = useState([]);
   const [SearchQuery, setSearchQuery] = useState('');
   const [filteredRecipes, setFilteredRecipes] = useState([]);
-  const isFocused = useIsFocused();
   const [searchTimeout, setSearchTimeout] = useState(null);
+  const isFocused = useIsFocused();
   const URL = 'https://lifemiam-backend.vercel.app'
-  
+
 
   const fetchPopularRecipes = () => {
     fetch(`${URL}/recipes/?&sortBy=popularity`)
@@ -34,7 +34,7 @@ export default function SearchScreen({ navigation }) {
         console.error('Error fetching data:', error);
       })
   }
-       
+
   useEffect(() => {
     fetchPopularRecipes();
   }, [isFocused]);
@@ -56,7 +56,7 @@ export default function SearchScreen({ navigation }) {
       });
   };
 
-  
+
   const handleSearch = (query) => {
     setSearchQuery(query);
     if (searchTimeout) {
@@ -70,71 +70,68 @@ export default function SearchScreen({ navigation }) {
   };
 
 
-
   const handleRecipeClick = (id) => {
     const updatedRecipes = recipes.map((recette) =>
       recette.id === id ? { ...recipes, clics: recette.clics + 1 } : recipes
     );
     setRecipes(updatedRecipes);
     setFilteredRecipes(updatedRecipes);
-    navigation.navigate("Recipe", {RecetteID: id});
+    navigation.navigate("Recipe", { RecetteID: id });
   };
+
 
 
   const popularRecipes = recipes.map((element, i) => {
     // console.log(element.image)
     return (
       <TouchableOpacity key={i} onPress={() => handleRecipeClick(element._id)}>
-
         <View style={styles.recipes}>
-
           <Image source={{ uri: element.image }} style={styles.recipeImage} />
           <Text style={styles.H3}>{element.name}</Text>
           <View style={styles.PHbutton}></View>
         </View>
-
       </TouchableOpacity>
     )
   })
+
+
   return (
     <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
-
       <View style={styles.titleCont}>
-
         <Text style={styles.H1}>Recettes</Text>
-
         <TextInput
           style={styles.searchBar}
           placeholder="Rechercher..."
           value={SearchQuery}
           onChangeText={handleSearch}
         />
-
         <Text style={styles.H2}>Les recettes populaires</Text>
-
-        <ScrollView style={styles.ScrollCont}> 
-
-          
+        <ScrollView style={styles.ScrollCont}>
           {popularRecipes}
-          
         </ScrollView>
       </View>
-      
-      {/* <FlatList
-        data={filteredRecipes}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id.toString()}
-      /> */}
     </KeyboardAvoidingView>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  titleCont:{
+  titleCont: {
     marginTop: 50,
     alignItems: "flex-start",
     marginLeft: "4%",
@@ -147,51 +144,51 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     width: "96%",
   },
-  H1:{
+  H1: {
     fontSize: 30,
-    color:"#365E32",
+    color: "#365E32",
     fontWeight: "700"
-},
-  H2:{
+  },
+  H2: {
     margin: 10,
     fontSize: 20,
-    color:"#365E32",
+    color: "#365E32",
     fontWeight: "700",
-},
-H3:{
+  },
+  H3: {
     marginLeft: 15,
     margin: 5,
     fontSize: 15,
-    color:"#365E32",
+    color: "#365E32",
     fontWeight: "700",
-},
-ScrollCont:{
-  width: "96%"
-},
-recipes:{
-  borderWidth:2,
-  borderColor: "#81A263",
-  height: 50,
-  flexDirection: "row",
-  alignItems: "center",
-  justifyContent: "space-between",
-  marginVertical: 10,
-  borderBottomLeftRadius: 30,
-  borderRadius: 10,
-},
-recipeImage: {
+  },
+  ScrollCont: {
+    width: "96%"
+  },
+  recipes: {
+    borderWidth: 2,
+    borderColor: "#81A263",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginVertical: 10,
+    borderBottomLeftRadius: 30,
+    borderRadius: 10,
+  },
+  recipeImage: {
     height: 60,
     width: 60,
     borderBottomLeftRadius: 30,
     borderTopRightRadius: 30,
-},
-PHbutton:{
-  borderWidth: 1,
-  borderRadius: 10,
-  backgroundColor: "green",
-  width: 50,
-  height: 50,
-}
+  },
+  PHbutton: {
+    borderWidth: 1,
+    borderRadius: 10,
+    backgroundColor: "green",
+    width: 50,
+    height: 50,
+  }
 });
 
 
