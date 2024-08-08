d9169b77e3881bb6d05fe35ec61ec1f489d75c9c
ThierryPa <papouchado.thierry@gmail.com>
2024-08-08 11:03:22 +0200

    Updated Signin with some style and navigation working now

--- components/Signin.js
+++ components/Signin.js
@@ -16,6 +16,9 @@ import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
+// Requested to navigate from a component
+import { useNavigation } from '@react-navigation/native';
+
 import { token } from '../reducers/user';
 
 function Signin() {
@@ -24,6 +27,8 @@ function Signin() {
   const [signin, setSignin] = useState(null);
   const [password, SetPassword] = useState(null);
   const [showPassword, setShowPassword] = useState(false);
+
+  const navigation = useNavigation(); // Requested to navigate from a component
 
   const handleSignin = () => {
     fetch(`${URL}/users/signin`, {
@@ -37,6 +42,7 @@ function Signin() {
 					dispatch(token({ token: data.token }));
 					setSignin('');
 					SetPassword('');
+          navigation.navigate("TabNavigator", { screen: "Search" });
 				}
         else {
           // Display an error
@@ -52,17 +58,19 @@ function Signin() {
       <Text style={styles.title}>Se connecter</Text>
       <KeyboardAvoidingView style={styles.inputContainer}>
           <Text style={styles.label}>Adresse email ou Username</Text>
-          <TextInput label={'Adresse email ou Username'} textContentType='email' keyboardType='email-address' placeholder='Username or email' style={styles.input} onChangeText={(e) => setSignin(e)} value={signin} maxLength={254}></TextInput>
+          <TextInput label={'Adresse email ou Username'} textContentType='email' keyboardType='email-address' style={styles.input} onChangeText={(e) => setSignin(e)} value={signin} maxLength={254}></TextInput>
       </KeyboardAvoidingView>
       <KeyboardAvoidingView style={styles.inputContainer}>
       <Text style={styles.label}>Password</Text>
-        <TextInput label={'Password'} textContentType='password' secureTextEntry={true} placeholder='Password' style={styles.input} onChangeText={(e) => SetPassword(e)} value={showPassword ? password : password} maxLength={128}></TextInput>
+        <TextInput label={'Password'} textContentType='password' secureTextEntry={showPassword ? false : true } style={styles.input} onChangeText={(e) => SetPassword(e)} value={showPassword ? password : password} maxLength={128}></TextInput>
         <View style={styles.showpassword}>
             {!showPassword && <FontAwesome name={'eye'} style={styles.icon} size={20} onPress={() => { setShowPassword(true)}}/>}
             {showPassword && <FontAwesome name={'eye-slash'} style={styles.icon} size={20} onPress={() =>{ setShowPassword(false)}}/>}
           </View>
       </KeyboardAvoidingView>
-        <TouchableOpacity style={styles.signinButton} onPress={() => handleSignin()}><Text style={styles.textButt}>Connexion</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.signinButton} onPress={() => handleSignin()}>
+          <Text style={styles.signinButtonText}>Connexion</Text>
+        </TouchableOpacity>
     </View>
   </SafeAreaView>
   )
@@ -87,7 +95,9 @@ const styles = StyleSheet.create({
       objectFit:'contain',
   },
   title: {
-
+    fontSize:36,
+    fontWeight:'500',
+    color:Colors.DARK_GREEN,
   },
   inputContainer: {
     position: 'relative',
@@ -122,14 +132,20 @@ const styles = StyleSheet.create({
     paddingHorizontal:10,
   },
   signinButton: {
-    backgroundColor: Colors.LIGHT_GREEN,
-    width: 100,
-    height: 100,
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:30,
+    alignSelf:'center',
+    borderWidth:1,
+    paddingHorizontal:40,
+    paddingVertical:10,
+    borderRadius:20,
+    width:'80%',
+    backgroundColor:Colors.DARK_GREEN,
   },
-  textButt: {
-    color: "white",
+  signinButtonText:
+  {
+    textAlign:'center',
+    color:Colors.YELLOW,
+    fontSize:16
   },
   password: {
     color: "black",
