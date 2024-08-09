311ed00d8bd256ea6908228591ef5535e304a7eb
Elisa <elisafan8@gmail.com>
2024-08-09 16:12:11 +0200

    fixing user reducer to debug SearchScreen, need to pull sign up fixes

--- components/Signin.js
+++ components/Signin.js
@@ -8,18 +8,18 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
-  ScrollView
-} from 'react-native';
-import Colors from '../utilities/color';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
+  ScrollView,
+} from "react-native";
+import Colors from "../utilities/color";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
 
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
-import { addRegime, token } from '../reducers/user';
+import { addRegime, token, initRegimes } from "../reducers/user";
 
-function Signin({navigation}) {
-  const URL = 'https://lifemiam-backend.vercel.app';
+function Signin({ navigation }) {
+  const URL = "https://lifemiam-backend.vercel.app";
   const dispatch = useDispatch();
   const [signin, setSignin] = useState(null);
   const [password, SetPassword] = useState(null);
@@ -27,131 +27,170 @@ function Signin({navigation}) {
 
   const handleSignin = () => {
     fetch(`${URL}/users/signin`, {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ signin: signin, password: password }),
-		}).then(response => response.json())
-			.then(data => {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ signin: signin, password: password }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
         console.log(data);
-				if (data.result === true) {
-					dispatch(token({ token: data.token }));
-          dispatch(addRegime({regime: data.regime}));
-					setSignin('');
-					SetPassword('');
+        if (data.result === true) {
+          // dispatch(token({ token: data.token }));
+          // dispatch(addRegime({regime: data.regime}));
+          dispatch(token(data.token));
+          data.regime.length > 0 && dispatch(initRegimes(data.regime));
+          setSignin("");
+          SetPassword("");
           navigation.navigate("TabNavigator", { screen: "Search" });
-				}
-        else {
+        } else {
           // Display an error
           console.log("Erreur avec Signin");
         }
-			});
-  }
+      });
+  };
   return (
-  <SafeAreaView style={styles.container}>
-    <View style={styles.logoContainer}>
-      <Image style={styles.logoImg} source={require("../assets/logo.png")}/>
-    </View>
-    <View style={styles.container}>
-      <Text style={styles.title}>Se connecter</Text>
-      <KeyboardAvoidingView style={styles.inputContainer}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logoImg} source={require("../assets/logo.png")} />
+      </View>
+      <View style={styles.container}>
+        <Text style={styles.title}>Se connecter</Text>
+        <KeyboardAvoidingView style={styles.inputContainer}>
           <Text style={styles.label}>Adresse email ou Username</Text>
-          <TextInput label={'Adresse email ou Username'} textContentType='email' keyboardType='email-address' style={styles.input} onChangeText={(e) => setSignin(e)} value={signin} maxLength={254}></TextInput>
-      </KeyboardAvoidingView>
-      <KeyboardAvoidingView style={styles.inputContainer}>
-      <Text style={styles.label}>Password</Text>
-        <TextInput label={'Password'} textContentType='password' secureTextEntry={showPassword ? false : true } style={styles.input} onChangeText={(e) => SetPassword(e)} value={showPassword ? password : password} maxLength={128}></TextInput>
-        <View style={styles.showpassword}>
-            {!showPassword && <FontAwesome name={'eye'} style={styles.icon} size={20} onPress={() => { setShowPassword(true)}}/>}
-            {showPassword && <FontAwesome name={'eye-slash'} style={styles.icon} size={20} onPress={() =>{ setShowPassword(false)}}/>}
+          <TextInput
+            label={"Adresse email ou Username"}
+            textContentType="email"
+            keyboardType="email-address"
+            style={styles.input}
+            onChangeText={(e) => setSignin(e)}
+            value={signin}
+            maxLength={254}
+          ></TextInput>
+        </KeyboardAvoidingView>
+        <KeyboardAvoidingView style={styles.inputContainer}>
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            label={"Password"}
+            textContentType="password"
+            secureTextEntry={showPassword ? false : true}
+            style={styles.input}
+            onChangeText={(e) => SetPassword(e)}
+            value={showPassword ? password : password}
+            maxLength={128}
+          ></TextInput>
+          <View style={styles.showpassword}>
+            {!showPassword && (
+              <FontAwesome
+                name={"eye"}
+                style={styles.icon}
+                size={20}
+                onPress={() => {
+                  setShowPassword(true);
+                }}
+              />
+            )}
+            {showPassword && (
+              <FontAwesome
+                name={"eye-slash"}
+                style={styles.icon}
+                size={20}
+                onPress={() => {
+                  setShowPassword(false);
+                }}
+              />
+            )}
           </View>
-      </KeyboardAvoidingView>
-        <TouchableOpacity style={styles.signinButton} onPress={() => handleSignin()}>
+        </KeyboardAvoidingView>
+        <TouchableOpacity
+          style={styles.signinButton}
+          onPress={() => handleSignin()}
+        >
           <Text style={styles.signinButtonText}>Connexion</Text>
         </TouchableOpacity>
-    </View>
-  </SafeAreaView>
-  )
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Colors.WHITE,
-    alignItems: 'center',
-    paddingTop:'10%',
-    width: '100%',
+    alignItems: "center",
+    paddingTop: "10%",
+    width: "100%",
   },
   logoContainer: {
-    height:'30%',
-    width:'100%',
+    height: "30%",
+    width: "100%",
   },
   logoImg: {
     borderWidth: 1,
-    width:'100%',
-    height:'100%',      
-    objectFit:'contain',
+    width: "100%",
+    height: "100%",
+    objectFit: "contain",
   },
   title: {
-    fontSize:36,
-    fontWeight:'500',
-    color:Colors.DARK_GREEN,
+    fontSize: 36,
+    fontWeight: "500",
+    color: Colors.DARK_GREEN,
   },
   inputContainer: {
-    position: 'relative',
+    position: "relative",
     borderWidth: 1,
     width: "80%",
-    marginTop:15,
-    marginBottom:15,
+    marginTop: 15,
+    marginBottom: 15,
   },
   label: {
-    position:'absolute',
-    top:-10,
-    left:10,
-    backgroundColor:Colors.WHITE,
-    color:Colors.LIGHT_GREEN,
-    zIndex:10,
-    paddingHorizontal:5,
-    fontWeight:'500'
+    position: "absolute",
+    top: -10,
+    left: 10,
+    backgroundColor: Colors.WHITE,
+    color: Colors.LIGHT_GREEN,
+    zIndex: 10,
+    paddingHorizontal: 5,
+    fontWeight: "500",
   },
   input: {
     height: 50,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     color: "black",
     borderWidth: 1,
-    borderColor:Colors.DARK_GREEN,
+    borderColor: Colors.DARK_GREEN,
     paddingHorizontal: 10,
   },
   showpassword: {
-    position:'absolute',
-    right:0,
-    height:50,
-    width:'20%',
-    alignItems:'flex-end',
-    justifyContent:'center',
-    paddingHorizontal:10,
+    position: "absolute",
+    right: 0,
+    height: 50,
+    width: "20%",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    paddingHorizontal: 10,
   },
-  icon:{
-    color:Colors.DARK_GREEN,
-    },
+  icon: {
+    color: Colors.DARK_GREEN,
+  },
   signinButton: {
-    marginTop:30,
-    alignSelf:'center',
-    borderWidth:1,
-    paddingHorizontal:40,
-    paddingVertical:10,
-    borderRadius:20,
-    width:'80%',
-    backgroundColor:Colors.DARK_GREEN,
+    marginTop: 30,
+    alignSelf: "center",
+    borderWidth: 1,
+    paddingHorizontal: 40,
+    paddingVertical: 10,
+    borderRadius: 20,
+    width: "80%",
+    backgroundColor: Colors.DARK_GREEN,
   },
-  signinButtonText:{
-    textAlign:'center',
-    color:Colors.YELLOW,
-    fontSize:16
+  signinButtonText: {
+    textAlign: "center",
+    color: Colors.YELLOW,
+    fontSize: 16,
   },
   password: {
     color: "black",
-  }
+  },
 });
 
 export default Signin;
--- reducers/user.js
+++ reducers/user.js
@@ -11,6 +11,9 @@ export const userSlice = createSlice({
     token: (state, action) => {
       state.value.token = action.payload;
     },
+    initRegimes: (state, action) => {
+      state.value.regime = action.payload;
+    },
     addRegime: (state, action) => {
       state.value.regime.push(action.payload);
       console.log("added", action.payload);
@@ -24,5 +27,6 @@ export const userSlice = createSlice({
   },
 });
 
-export const { token, addRegime, removeRegime } = userSlice.actions;
+export const { token, addRegime, removeRegime, initRegimes } =
+  userSlice.actions;
 export default userSlice.reducer;
