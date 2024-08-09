3eac00fc71dd984479d2118b59e57736d9613a95
jérôme <jplusquellec@gmail.com>
2024-08-09 18:03:31 +0200

    listScreen en dynamique

--- screens/ListScreen.js
+++ screens/ListScreen.js
@@ -1,271 +1,181 @@
 import {
-    View,
-    Image,
-    KeyboardAvoidingView,
-    Platform,
-    StyleSheet,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    FlatList,
-    ScrollView
-  } from 'react-native';
-
+  View,
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  FlatList,
+  ScrollView
+} from 'react-native';
+import { useState, useEffect } from 'react';
 import Colors from "../utilities/color";
-
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { useDispatch, useSelector } from "react-redux";
 
-// qu'est ce qu'on fait !!!!!!!!!
+export default function ListScreen({ navigation: { goBack } }) {
 
-// récupérer les datas en dur puis par fetch 
-// mapper le tableau datas <View>name - quantity - unité</View>
-// trier l'affichage par catégories
-// dynamiser la liste 
-//  - par défaut l'ingrédient est dans une liste 'à acheter'
-// - swipe a droite : l'ingrédient passe dans liste produits achetés 
+  // récupérer les catégories ['fruits','légumes','produits laitiers','produits secs','Condiments','Boissons']
+  // filtre le tableau datas.data et extraire les catégories et supprimer les doublons
+  // parcourir le tableau des catégories
+  // faire le map d'affichage
+  const URL = "https://lifemiam-backend.vercel.app";
+  // const URL = "http://localhost:3000";
+  // const token = "wVL5sCx7YTgaO-fnxK5pX4mMG8JywAwQ"
+  const [list, setList] = useState([]);
+  const {token, menuId} = useSelector((state) => state.user.value.token);
+  const dispatch = useDispatch();
 
-let datas={
-  "result": true,
-  "data": [
-    {
-      "name": "Pommes",
-      "quantity": 24,
-      "unit": "unités",
-      "category": "Fruits"
-    },
-    {
-      "name": "Bananes",
-      "quantity": 12,
-      "unit": "unités",
-      "category": "Fruits"
-    },
-    {
-      "name": "Fraises",
-      "quantity": 800,
-      "unit": "grammes",
-      "category": "Fruits"
-    },
-    {
-      "name": "Tomates cerises",
-      "quantity": 400,
-      "unit": "grammes",
-      "category": "Légumes"
-    },
-    {
-      "name": "Bananes",
-      "quantity": 12,
-      "unit": "unités",
-      "category": "Fruits"
-    },
-    {
-      "name": "Fraises",
-      "quantity": 800,
-      "unit": "grammes",
-      "category": "Fruits"
-    },
-    {
-      "name": "Lait d'amande",
-      "quantity": 2,
-      "unit": "litres",
-      "category": "Boissons"
-    },
-    {
-      "name": "Pommes de terre",
-      "quantity": 8,
-      "unit": "unités",
-      "category": "Légumes"
-    },
-    {
-      "name": "Courgettes",
-      "quantity": 8,
-      "unit": "unités",
-      "category": "Légumes"
-    },
-    {
-      "name": "Carottes",
-      "quantity": 12,
-      "unit": "unités",
-      "category": "Légumes"
-    },
-    {
-      "name": "Champignons",
-      "quantity": 400,
-      "unit": "grammes",
-      "category": "Légumes"
-    },
-    {
-      "name": "Ail",
-      "quantity": 8,
-      "unit": "gousses",
-      "category": "Condiments"
-    },
-    {
-      "name": "Huile d'olive",
-      "quantity": 12,
-      "unit": "cuillères à soupe",
-      "category": "Condiments"
-    },
-    {
-      "name": "Farine",
-      "quantity": 800,
-      "unit": "grammes",
-      "category": "Produits secs"
-    },
-    {
-      "name": "Sucre",
-      "quantity": 408,
-      "unit": "grammes",
-      "category": "Produits secs"
-    },
-    {
-      "name": "Beurre",
-      "quantity": 200,
-      "unit": "grammes",
-      "category": "Produits laitiers"
-    },
-    {
-      "name": "Pommes",
-      "quantity": 24,
-      "unit": "unités",
-      "category": "Fruits"
-    },
-    {
-      "name": "Sucre",
-      "quantity": 408,
-      "unit": "grammes",
-      "category": "Produits secs"
+  useEffect(() => {
+    if (token && menuId) {
+    fetchIngredients(token, menuId);
     }
-  ]
-}
-  
-  export default function ListScreen({navigation:{goBack}}) {
-
-      // récupérer les catégories ['fruits','légumes','produits laitiers','produits secs','Condiments','Boissons']
-        // filtre le tableau datas.data et extraire les catégories et supprimer les doublons
-        // parcourir le tableau des catégories
-          // faire le map d'affichage
-         const categories = []
-         datas.data.filter((e) => !categories.find((cat) => cat === e.category) ? categories.push(e.category) : null ) 
+  }, [token, menuId]);
+  console.log(token)
+  const fetchIngredients = (token, menuId) => {
+    fetch(`${URL}/shop/generate/${menuId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ token })
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data)
+      if (data.result) {
+        dispatch(setList(data.data));
+      } else {
+        console.error("No matching ingredients");
+      }
+    })
+    .catch((error) => {
+      console.error("Error fetching search results:", error);
+    });
+  };
         
-         categories.sort()
-         console.log(categories)
+  const categories = []
+  list.filter((e) => !categories.find((cat) => cat === e.category) ? categories.push(e.category) : null)
 
-         let displayAll=[]
-          for (let category of categories)
-          {
-            
-            let displayList=datas.data.map((ing,i) => ing.category == category ?
-            <View key={i} style={styles.ingElement}> 
-              <Text style={styles.ingElementText}>{ing.name} {ing.quantity} {ing.unit }</Text>
-              </View> : null 
-              ) 
-              let boxCategory=
-              <View key={category} style={styles.boxCategory}>
-              <View key={category} style={styles.categorieTitle}><Text style={styles.categorieTitleText}>{category}</Text></View>
-              {displayList}
-              </View>
-              displayAll=[...displayAll,boxCategory]
-          }
-         
+  categories.sort()
+  console.log(categories)
 
-    return (
-      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
+  let displayAll = []
+  for (let category of categories) {
+
+    let displayList = list.map((ing, i) => ing.category == category ?
+      <View key={i} style={styles.ingElement}>
+        <Text style={styles.ingElementText}>{ing.name} {ing.quantity} {ing.unit}</Text>
+      </View> : null
+    )
+    let boxCategory =
+      <View key={category} style={styles.boxCategory}>
+        <View key={category} style={styles.categorieTitle}><Text style={styles.categorieTitleText}>{category}</Text></View>
+        {displayList}
+      </View>
+    displayAll = [...displayAll, boxCategory]
+  }
+  
+  return (
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.container}>
 
       <View style={styles.titleCont}>
+        
         <TouchableOpacity onPress={() => goBack()} activeOpacity={0.6} >
-        <FontAwesome name={'arrow-left'} style={styles.iconBack} size={30}/>
+          <FontAwesome name={'arrow-left'} style={styles.iconBack} size={30} />
         </TouchableOpacity>
-      
+
         <Text style={styles.H1}>Liste de courses</Text>
 
         
         <TouchableOpacity activeOpacity={0.6}>
-        <FontAwesome name={'sort-amount-desc'} style={styles.iconSort} size={30}/>
+          <FontAwesome name={'sort-amount-desc'} style={styles.iconSort} size={30} />
         </TouchableOpacity>
-   
-      </View>   
-        <ScrollView style={styles.list}>
-          
+
+      </View>
+      <ScrollView style={styles.list}>
+
         {displayAll}
-    
 
-        </ScrollView>
-      </KeyboardAvoidingView>
-    )
+
+      </ScrollView>
+    </KeyboardAvoidingView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center'
+
+  },
+  titleCont: {
+    marginTop: 50,
+    alignItems: "center",
+    justifyContent: "space-between",
+
+    flexDirection: 'row',
+    paddingHorizontal: 10,
+
+    width: '95%',
+    marginBottom: 20
+  },
+  H1: {
+    fontSize: 30,
+    color: "#365E32",
+    fontWeight: "700"
+  },
+
+  list: {
+    height: '90%',
+    width: '90%',
+
+    alignSelf: 'center'
+  },
+  boxCategory:
+  {
+    marginBottom: 20
+  },
+  categorieTitle:
+  {
+    marginBottom: 5
+  },
+  categorieTitleText:
+  {
+    fontSize: 30,
+    color: Colors.DARK_GREEN,
+  },
+  ingElement:
+  {
+    borderWidth: 1,
+    width: '100%',
+    height: 80,
+    color: 'red',
+
+  },
+  ingElementText:
+  {
+
+  },
+  iconBack:
+  {
+    backgroundColor: Colors.DARK_GREEN,
+    color: Colors.YELLOW,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 100,
+    paddingHorizontal: 12
+  },
+  iconSort:
+  {
+
+    color: Colors.DARK_GREEN,
+
+    padding: 10,
+
+    paddingHorizontal: 12
   }
-  
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems:'center'
-
-    },
-    titleCont: {
-      marginTop: 50,
-      alignItems: "center",
-      justifyContent:"space-between",
-
-      flexDirection:'row',
-      paddingHorizontal:10,
-
-      width:'95%',
-      marginBottom:20
-    },
-    H1: {
-      fontSize: 30,
-      color: "#365E32",
-      fontWeight: "700"
-    },
-
-    list: {
-      height: '90%',
-      width: '90%',
-     
-      alignSelf:'center'
-    },
-    boxCategory:
-    {
-      marginBottom:20
-    },
-    categorieTitle:
-    {
-    marginBottom:5
-    },
-    categorieTitleText:
-    {
-    fontSize:30,
-    color:Colors.DARK_GREEN,
-    },
-    ingElement:
-    {
-      borderWidth:1,
-      width: '100%',
-      height:80,
-      color:'red',
-      
-    },
-    ingElementText:
-    {
-    
-    },
-    iconBack:
-    {
-      backgroundColor:Colors.DARK_GREEN,
-      color:Colors.YELLOW,
-      borderWidth:1,
-      padding:10,
-      borderRadius:100,
-      paddingHorizontal:12
-    },
-    iconSort:
-    {
-     
-      color:Colors.DARK_GREEN,
-     
-      padding:10,
-     
-      paddingHorizontal:12
-    }
-  });
-  
+});
