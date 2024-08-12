9047186ec0f36e203b5f25dd4ff9f533e2da768e
ThierryPa <papouchado.thierry@gmail.com>
2024-08-12 13:03:48 +0200

    Frenchisation d'onboarding / resume / signin

--- components/Onboarding.js
+++ components/Onboarding.js
@@ -25,7 +25,7 @@ function Onboarding({navigation,page}) {
 
   const dispatch = useDispatch();
   const userToken = useSelector((state) => state.user.value.token);
-  const token = "-ob9kC9qgjQ7vn9vx0s6JUGmtlUWANtv";
+  //const token = "-ob9kC9qgjQ7vn9vx0s6JUGmtlUWANtv";
   const userRegime = useSelector((state) => state.user.value.regime);
   const URL = "https://lifemiam-backend.vercel.app";
 
@@ -70,7 +70,7 @@ function Onboarding({navigation,page}) {
       fetch(`${URL}/users/update`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ token: token, regime: userRegime }),
+        body: JSON.stringify({ token: userToken, regime: userRegime }),
       })
         .then((response) => response.json())
         .then((data) => {
@@ -84,8 +84,8 @@ function Onboarding({navigation,page}) {
 
   return (
     <SafeAreaView style={styles.container}>
-      {page == 'onboarding' && <Text style={styles.title}>Complete ton profil</Text>}
-      {page == 'profile' && <Text style={styles.title}>Modifie ton profil</Text>}
+      {page == 'onboarding' && <Text style={styles.title}>Complète ton profil</Text>}
+      {page == 'profile' && <Text style={styles.title}>Modifier ton profil</Text>}
       <Text style={styles.question}>Des restrictions alimentaires ?</Text>
       <View style={styles.vignetteContainer}>{regimeVignettes}</View>
       <View style={styles.buttons}>
